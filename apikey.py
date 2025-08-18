56802656756840f9adb6dacd0b0707a5b2ed5177
Dereje <dereje.tekola@gmail.com>
2025-08-18 19:03:54 -0400

    addedd a comment about URL

--- apikey.py
+++ apikey.py
@@ -20,6 +20,7 @@ import ast
 PASSWORD = getpass.getpass()
 # url for each device that the key is generated
 url = "https://<URL>"
+# Replace URL with the IP or FQDN of the device
 
 # refer RestAPI documentation https://<url>/restapi-doc
 
