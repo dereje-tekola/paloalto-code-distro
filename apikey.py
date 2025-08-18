f5248acf5f839de105c5c32da34736ff445f0a91
Dereje <dereje.tekola@gmail.com>
2025-08-18 18:57:42 -0400

    new file to get restapi key

---
+++ apikey.py
@@ -0,0 +1,34 @@
+# Author: Tekola
+# Date: March 19, 2024
+# Version: 1
+
+import sys
+import json
+import requests
+import getpass
+import panos
+import urllib3
+from panos import panorama, firewall, network, objects
+import ast
+
+# API account without Commit privilege should be created on the device
+# API account name used here is -- ******** --
+# API account password = PASSWORD = "************"
+# For the https cert validation, add/append the Root/Intermediate(s) of the site into the location
+# .../lib/python3.9/site-packages/certifi/cacert.pem
+
+PASSWORD = getpass.getpass()
+# url for each device that the key is generated
+url = "https://<URL>"
+
+# refer RestAPI documentation https://<url>/restapi-doc
+
+url_plus_parameters = url+"/api/?type=keygen&user=devops&password="+PASSWORD
+response = requests.get(url_plus_parameters, verify=True)
+print()
+print(response)
+
+APIKEY = response.text.split("<key>")[1].split("</key>")[0]
+
+print(APIKEY)
+print()
